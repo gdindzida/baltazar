d1603e41fc6dff85bc16a74242ddde4439e49f1b
gdindzida <gdindzida@gmailcom.com>
2025-08-16 00:46:45 +0200

    Simplify thread pool impl

--- src/thread_pool/test/thread_pool_test.cpp
+++ src/thread_pool/test/thread_pool_test.cpp
@@ -29,7 +29,7 @@ TEST(ThreadPoolTest, CreateThreadsWithNoTasksAndWaitForAll) {
   constexpr size_t numThreads = 2;
 
   // Act
-  threadPool::ThreadPool<numThreads, 0, 10> threadPool{};
+  threadPool::ThreadPool<numThreads, 10> threadPool{};
   std::atomic stop{false};
   threadPool.waitForAllTasks(stop);
 
@@ -39,7 +39,7 @@ TEST(ThreadPoolTest, CreateThreadsWithNoTasksAndWaitForAll) {
 TEST(ThreadPoolTest, CreateThreadsWithOneTaskAndWaitForAll) {
   // Arrange
   constexpr size_t numThreads = 2;
-  threadPool::ThreadPool<numThreads, 0, 10> threadPool{};
+  threadPool::ThreadPool<numThreads, 10> threadPool{};
   TestThreadTask task{nullptr};
 
   // Act
@@ -57,7 +57,7 @@ TEST(ThreadPoolTest, CreateThreadsWithManyTasksAndWaitForAll) {
   constexpr size_t numOfTasks = 300;
   std::atomic<size_t> testCounter{0};
 
-  threadPool::ThreadPool<numThreads, 0, 10> threadPool{};
+  threadPool::ThreadPool<numThreads, 10> threadPool{};
   TestThreadTask task{&testCounter};
 
   // Act
@@ -85,7 +85,7 @@ TEST(ThreadPoolTest, CreateThreadsWithManyTasksAndWaitForAllButStopEarly) {
   constexpr size_t numOfTasks = 300;
   std::atomic<size_t> testCounter{0};
 
-  threadPool::ThreadPool<numThreads, 0, 10> threadPool{};
+  threadPool::ThreadPool<numThreads, 10> threadPool{};
   TestThreadTask task{&testCounter};
 
   // Act
@@ -105,63 +105,4 @@ TEST(ThreadPoolTest, CreateThreadsWithManyTasksAndWaitForAllButStopEarly) {
 
   // Assert
   EXPECT_LE(counter, numOfTasks);
-}
-
-TEST(ThreadPoolTest, CreateThreadsWithPersitentTasksAndStop) {
-  // Arrange
-  constexpr size_t numThreads = 2;
-  std::atomic<size_t> testCounter{0};
-
-  // Act
-  TestThreadTask task{&testCounter};
-  threadPool::ThreadPool<numThreads, 3, 10> threadPool{{&task, &task, &task}};
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(4));
-
-  threadPool.shutdown();
-
-  // Assert
-  EXPECT_LE(testCounter.load(), 4);
-}
-
-TEST(ThreadPoolTest, CreateThreadsWithPersitentTasksThenAddFewTasksAndStop) {
-  // Arrange
-  constexpr size_t numThreads = 2;
-  std::atomic<size_t> testCounter{0};
-  std::atomic<size_t> testCounter1{0};
-
-  // Act
-  TestThreadTask task{&testCounter};
-  threadPool::ThreadPool<numThreads, 3, 10> threadPool{{&task, &task, &task}};
-
-  TestThreadTask additionalTask{&testCounter1};
-  threadPool.scheduleTask(&additionalTask);
-  threadPool.scheduleTask(&additionalTask);
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(6));
-
-  threadPool.shutdown();
-
-  // Assert
-  EXPECT_LE(testCounter.load(), 5);
-  EXPECT_LE(testCounter1.load(), 2);
-  EXPECT_GE(testCounter1.load(), 1);
-}
-
-TEST(ThreadPoolTest, CreateThreadsWithPersitentTasksNTimes) {
-  // Arrange
-  constexpr size_t numThreads = 2;
-  constexpr size_t numOfLoops = 15;
-  std::atomic<size_t> testCounter{0};
-
-  // Act
-  TestThreadTask task{&testCounter};
-  threadPool::ThreadPool<numThreads, 3, 10> threadPool{{&task, &task, &task},
-                                                       numOfLoops};
-
-  std::atomic<bool> stop{false};
-  threadPool.waitForAllTasks(stop);
-
-  // Assert
-  EXPECT_LE(testCounter.load(), 3 * numOfLoops);
 }
--- src/thread_pool/thread_pool.hpp
+++ src/thread_pool/thread_pool.hpp
@@ -11,157 +11,46 @@
 
 namespace threadPool {
 
-template <size_t THREAD_NUM, size_t PERSISTENT_QUEUE_SIZE,
-          size_t MAX_QUEUE_SIZE>
-class ThreadPool {
+template <size_t THREAD_NUM, size_t MAX_QUEUE_SIZE> class ThreadPool {
   std::array<std::thread, THREAD_NUM> m_threads;
-  std::array<IThreadTask *, PERSISTENT_QUEUE_SIZE> m_persistentTasks;
-  std::atomic<size_t> m_numberOfPersistentTasksDoneInCurrentIteration{
-      PERSISTENT_QUEUE_SIZE};
-  size_t m_currentPersistentTaskIndex{0};
-  int64_t m_currentPersistentLoopIteration{0};
-  int64_t m_maxNumberOfPersistentLoopIterations{-1};
   TaskQueue<MAX_QUEUE_SIZE> m_tasks;
   std::mutex m_mtx;
-  std::mutex m_mtxLoop;
   std::condition_variable m_addTaskCv;
   std::condition_variable m_finishTaskCv;
-  std::condition_variable m_finishedLoopCv;
   bool m_stop{false};
-  bool firstLoop{true};
 
-  void init() {
+public:
+  explicit ThreadPool() {
     for (int i = 0; i < THREAD_NUM; i++) {
       m_threads[i] = std::thread([this, i] {
         const IThreadTask *task = &nullThreadTask;
         while (true) {
           std::unique_lock lock(m_mtx);
-          m_addTaskCv.wait(lock, [this] {
-            return (PERSISTENT_QUEUE_SIZE > 0) || !m_tasks.empty() || m_stop;
-          });
+          m_addTaskCv.wait(lock, [this] { return !m_tasks.empty() || m_stop; });
+
           if (m_stop) {
             return;
           }
 
+#ifdef DEBUGLOG
           std::cout << "[Thread" << i << "] " << "Thread " << i
                     << " has resources." << "\n";
 
-          bool currentTaskIsPersistent = false;
-
           // Currently all non-persistent tasks are prioritized.
-          if (!m_tasks.empty()) {
-            std::cout << "[Thread" << i << "] "
-                      << "Running non persistent task = " << task->name()
-                      << "\n";
-            task = m_tasks.pop();
-          } else {
-            std::cout << "[Thread" << i << "] "
-                      << "Trying to run persistent task. id="
-                      << m_currentPersistentTaskIndex << "\n";
+          std::cout << "[Thread" << i << "] "
+                    << "Running a task = " << task->name() << "\n";
+#endif
 
-            if (m_currentPersistentTaskIndex == 0) {
-              std::cout << "[Thread" << i << "] "
-                        << "Trying to run new iteration. "
-                        << m_currentPersistentLoopIteration << " / "
-                        << m_maxNumberOfPersistentLoopIterations << "\n";
-
-              if (!firstLoop &&
-                  m_numberOfPersistentTasksDoneInCurrentIteration <
-                      PERSISTENT_QUEUE_SIZE) {
-                std::cout << "[Thread" << i << "] "
-                          << "Waiting for previous iteration to finish..."
-                          << "\n";
-
-                m_finishedLoopCv.wait(lock, [this] { return true; });
-
-                lock.unlock();
-
-                std::cout << "[Thread" << i << "] "
-                          << "Finished waiting!!!"
-                          << "\n";
-
-                continue;
-              }
-
-              if (firstLoop) {
-                firstLoop = false;
-              }
-
-              if (m_maxNumberOfPersistentLoopIterations > 0) {
-                if (m_currentPersistentLoopIteration <
-                    m_maxNumberOfPersistentLoopIterations) {
-                  m_currentPersistentLoopIteration++;
-                } else {
-                  std::cout << "[Thread" << i << "] "
-                            << "All persistent loops are done."
-                            << "\n";
-                  lock.unlock();
-                  continue;
-                }
-              }
-
-              std::cout << "[Thread" << i << "] "
-                        << "Starting new iteration." << "\n";
-
-              // reset number of tasks done.
-              m_numberOfPersistentTasksDoneInCurrentIteration = 0;
-            }
-
-            task = m_persistentTasks[m_currentPersistentTaskIndex];
-            currentTaskIsPersistent = true;
-
-            std::cout << "[Thread" << i << "] "
-                      << "Running persistent task =" << task->name() << "\n";
-
-            m_currentPersistentTaskIndex++;
-            m_currentPersistentTaskIndex %= PERSISTENT_QUEUE_SIZE;
-          }
+          task = m_tasks.pop();
 
           lock.unlock();
+
           task->run();
+
           m_finishTaskCv.notify_all();
-          if (currentTaskIsPersistent) {
-            ++m_numberOfPersistentTasksDoneInCurrentIteration;
-
-            if (m_numberOfPersistentTasksDoneInCurrentIteration >=
-                PERSISTENT_QUEUE_SIZE) {
-              m_finishedLoopCv.notify_all();
-            }
-          }
-
-          std::cout << "[Thread" << i << "] " << task->name() << " done. "
-                    << m_numberOfPersistentTasksDoneInCurrentIteration << " / "
-                    << PERSISTENT_QUEUE_SIZE << "\n";
         }
       });
     }
-  }
-
-  bool isPersistentLoopDone() const {
-    return (m_currentPersistentLoopIteration >=
-            m_maxNumberOfPersistentLoopIterations) &&
-           (m_numberOfPersistentTasksDoneInCurrentIteration >=
-            PERSISTENT_QUEUE_SIZE);
-  }
-
-public:
-  explicit ThreadPool(
-      const std::array<IThreadTask *, PERSISTENT_QUEUE_SIZE> &persistentTasks,
-      int64_t maxNumberOfPersistentLoops = -1) {
-    static_assert(PERSISTENT_QUEUE_SIZE > 0);
-
-    for (size_t i = 0; i < PERSISTENT_QUEUE_SIZE; ++i) {
-      m_persistentTasks[i] = persistentTasks[i];
-    }
-
-    m_maxNumberOfPersistentLoopIterations = maxNumberOfPersistentLoops;
-
-    init();
-  }
-
-  explicit ThreadPool() {
-    static_assert(PERSISTENT_QUEUE_SIZE == 0);
-    init();
   }
 
   ~ThreadPool() {
@@ -191,7 +80,7 @@ public:
     std::unique_lock lock(m_mtx);
     m_finishTaskCv.wait(lock, [this, &stopFlag] {
       m_stop = stopFlag;
-      return (m_tasks.empty() && isPersistentLoopDone()) || m_stop;
+      return m_tasks.empty() || m_stop;
     });
   }
 
@@ -199,9 +88,10 @@ public:
     std::unique_lock lock(m_mtx);
     m_stop = true;
 
+#ifdef DEBUGLOG
     std::cout << "Shutting down..." << "\n";
-    std::cout << "No persistent tasks = " << m_tasks.empty() << "\n";
-    std::cout << "Is persistent loop done = " << isPersistentLoopDone() << "\n";
+    std::cout << "No tasks = " << m_tasks.empty() << "\n";
+#endif
 
     lock.unlock();
     m_addTaskCv.notify_all();
