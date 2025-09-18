2010f52380c3b921e65bc5e5c9b232ec297ade4e
gdindzida <gdindzida@gmailcom.com>
2025-09-18 14:51:50 +0200

    Implement serial execution

--- src/core/core.hpp
+++ src/core/core.hpp
@@ -5,14 +5,14 @@
 #include <atomic>
 
 namespace core {
-template <size_t GRAPH_SIZE>
-void runGraphSerialOnce(dag::Dag<GRAPH_SIZE> &graph,
-                        std::atomic<bool> &stopFlag) {
-  const auto tasks = graph.getSortedTasks();
 
-  for (auto *threadTaskPtr : tasks) {
-
-    threadTaskPtr->run();
+template <size_t NUM_OF_NODES>
+void runNodeListSerialOnce(dag::NodeList<NUM_OF_NODES> nodes,
+                           std::atomic<bool> &stopFlag) {
+  for (size_t nodeIndex = 0; nodeIndex < nodes.getNumberOfNodes();
+       nodeIndex++) {
+    dag::INode *currentNode = nodes.getNodeAt(nodeIndex);
+    currentNode->execute();
 
     if (stopFlag) {
       break;
@@ -20,20 +20,11 @@ void runGraphSerialOnce(dag::Dag<GRAPH_SIZE> &graph,
   }
 }
 
-template <size_t GRAPH_SIZE>
-void runGraphSerialNTimes(dag::Dag<GRAPH_SIZE> &graph,
-                          std::atomic<bool> &stopFlag, size_t n) {
-  const auto tasks = graph.getSortedTasks();
-
+template <size_t NUM_OF_NODES>
+void runNodeListSerialNTimes(dag::NodeList<NUM_OF_NODES> nodes,
+                             std::atomic<bool> &stopFlag, size_t n) {
   for (int iter = 0; iter < n; iter++) {
-    for (auto *threadTaskPtr : tasks) {
-
-      threadTaskPtr->run();
-
-      if (stopFlag) {
-        break;
-      }
-    }
+    runNodeListSerialOnce(nodes, stopFlag);
 
     if (stopFlag) {
       break;
@@ -41,53 +32,53 @@ void runGraphSerialNTimes(dag::Dag<GRAPH_SIZE> &graph,
   }
 }
 
-template <size_t GRAPH_SIZE>
-void runGraphSerialLoop(dag::Dag<GRAPH_SIZE> &graph,
-                        std::atomic<bool> &stopFlag) {
-  const auto tasks = graph.getSortedTasks();
-
+template <size_t NUM_OF_NODES>
+void runNodeListSerialLoop(dag::NodeList<NUM_OF_NODES> nodes,
+                           std::atomic<bool> &stopFlag) {
   while (!stopFlag) {
-    for (auto *threadTaskPtr : tasks) {
-
-      threadTaskPtr->run();
-    }
+    runNodeListSerialOnce(nodes, stopFlag);
   }
 }
 
-template <size_t GRAPH_SIZE, size_t NUMBER_OF_THREADS, size_t TASK_BUFFER_SIZE>
-void runGraphParallelOnce(dag::Dag<GRAPH_SIZE> &graph,
-                          std::atomic<bool> &stopFlag) {
-  const auto tasks = graph.getSortedTasks();
+// template <size_t GRAPH_SIZE, size_t NUMBER_OF_THREADS, size_t
+// TASK_BUFFER_SIZE> void runGraphParallelOnce(dag::Dag<GRAPH_SIZE> &graph,
+//                           std::atomic<bool> &stopFlag) {
+//   const auto tasks = graph.getSortedTasks();
 
-  threadPool::ThreadPool<NUMBER_OF_THREADS, GRAPH_SIZE, TASK_BUFFER_SIZE> pool{
-      tasks, 1};
+//   threadPool::ThreadPool<NUMBER_OF_THREADS, GRAPH_SIZE, TASK_BUFFER_SIZE>
+//   pool{
+//       tasks, 1};
 
-  pool.waitForAllTasks(stopFlag);
-}
+//   pool.waitForAllTasks(stopFlag);
+// }
 
-template <size_t GRAPH_SIZE, size_t NUMBER_OF_THREADS, size_t TASK_BUFFER_SIZE>
-void runGraphParallelNTimes(dag::Dag<GRAPH_SIZE> &graph,
-                            std::atomic<bool> &stopFlag, size_t n,
-                            bool waitForPrevious, const size_t timeoutMillis) {
-  const auto tasks = graph.getSortedTasks();
+// template <size_t GRAPH_SIZE, size_t NUMBER_OF_THREADS, size_t
+// TASK_BUFFER_SIZE> void runGraphParallelNTimes(dag::Dag<GRAPH_SIZE> &graph,
+//                             std::atomic<bool> &stopFlag, size_t n,
+//                             bool waitForPrevious, const size_t timeoutMillis)
+//                             {
+//   const auto tasks = graph.getSortedTasks();
 
-  threadPool::ThreadPool<NUMBER_OF_THREADS, GRAPH_SIZE, TASK_BUFFER_SIZE> pool{
-      tasks, n};
+//   threadPool::ThreadPool<NUMBER_OF_THREADS, GRAPH_SIZE, TASK_BUFFER_SIZE>
+//   pool{
+//       tasks, n};
 
-  pool.waitForAllTasks(stopFlag);
-}
+//   pool.waitForAllTasks(stopFlag);
+// }
 
-template <size_t GRAPH_SIZE, size_t NUMBER_OF_THREADS, size_t TASK_BUFFER_SIZE>
-void runGraphParallelLoop(dag::Dag<GRAPH_SIZE> &graph,
-                          std::atomic<bool> &stopFlag, bool waitForPrevious,
-                          const size_t timeoutMillis) {
-  const auto tasks = graph.getSortedTasks();
+// template <size_t GRAPH_SIZE, size_t NUMBER_OF_THREADS, size_t
+// TASK_BUFFER_SIZE> void runGraphParallelLoop(dag::Dag<GRAPH_SIZE> &graph,
+//                           std::atomic<bool> &stopFlag, bool waitForPrevious,
+//                           const size_t timeoutMillis) {
+//   const auto tasks = graph.getSortedTasks();
 
-  threadPool::ThreadPool<NUMBER_OF_THREADS, GRAPH_SIZE, TASK_BUFFER_SIZE> pool{
-      tasks};
+//   threadPool::ThreadPool<NUMBER_OF_THREADS, GRAPH_SIZE, TASK_BUFFER_SIZE>
+//   pool{
+//       tasks};
 
-  pool.waitForAllTasks(stopFlag);
-}
+//   pool.waitForAllTasks(stopFlag);
+// }
+
 } // namespace core
 
 #endif // CORE_H
